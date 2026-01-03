782e6f12ee5737d3678ef7808e940790f732bf00
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-01-03 17:17:24 +0100

    Adding starting page

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Tui text reader using RSVP with ORP alignment.
 ```sh
 python3 main.py FILE1 [ FILE2, ...]
 ```
+
+## PDF version
+
+```sh
+python3 main.py [starting page] PDF1 
+```
+
--- pdfreader.py
+++ pdfreader.py
@@ -10,13 +10,20 @@ def main(stdscr):
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_RED, -1)
     curses.curs_set(0)  
+    startpage = 0
 
     while(len(sys.argv) > 1):
+        if (sys.argv[1].isdecimal()):
+            startpage = int(sys.argv.pop(1)) -1
+            continue
+
         orp_print(*orp("x"), stdscr)
         time.sleep(1)
+
         with PdfReader(sys.argv.pop(1)) as f:
-            for page in f.pages:
+            for page in f.pages[startpage:]:
                 read(page.extract_text(), 60.0 / wpm, stdscr);
+        startpage = 0
 
 if __name__ == '__main__':
     try:
